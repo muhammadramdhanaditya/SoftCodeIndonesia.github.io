123efa4da8ddc15c3869278c3dd329d8a94a3d4b
SoftCodeIndonesia <softcodeindonesia@gmail.com>
2019-12-27 14:52:45 +0700

    initial Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.page-scroll').click(function() {
+    console.log('ok');
+    // var href = $(this).attr('href');
+    // var elementTujuan = $(href);
+    // $("body").animate({
+    //   scrollTop: elementTujuan.offset().top
+    // }, 1250, 'swing');
+});
